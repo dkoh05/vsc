d88d21da7ee70ba420ea25feb6342ce7fd872600
dkoh05 <dykoh05@gmail.com>
2022-06-05 12:04:31 +0900

    fixing up the code
    added routing, garbage cleaning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import Body from "./components/body/body";
+
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
+import Home from "./pages/home";
+import About from "./pages/about";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Body />
+      <Routes>
+        <Route path="/" element={ <Home /> }/>
+        <Route path="/about" element={ <About /> }/>
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,30 @@
-function Header() {
-    return (
-        <div>
-            This is a header
-        </div>
-    )
-}
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import "./header.css"
 
-export default Header
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            DaeYeon's Visual Portfolio
+          </Typography>
+          <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="about">About</Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
